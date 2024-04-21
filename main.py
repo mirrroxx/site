fca2de06ad294989c1984dffac7766631687b4d0
mirrroxx <astakhov2547@yandex.ru>
2024-04-22 01:09:49 +0700

    Добавил главную страницу

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("main.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/breakfast')
+def breakfast():
+    return render_template("breakfast.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
