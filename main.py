05bc16ca2a63767b94fffc6c87b6abca196e4e10
Egor <EgorTarasovbrn@yandex.ru>
2024-04-23 17:13:41 +0700

    коммит

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-from flask import Flask, render_template, url_for
+
+from flask import Flask, render_template, url_for, redirect
+from data import db_session
+from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('templates/register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/')
@@ -19,4 +48,5 @@ def breakfast():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/main.db")
     app.run(debug=True)
