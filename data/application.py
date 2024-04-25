4e4c9a3337bdb465930c31427599969534f44896
mirrroxx <astakhov2547@yandex.ru>
2024-04-26 03:37:13 +0700

    ф

---
+++ data/application.py
@@ -0,0 +1,32 @@
+import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from .db_session import SqlAlchemyBase
+
+
+class Application(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'application'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    grade = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    balance = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+
+    role = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+    #
+    # def __repr__(self):
+    #     return f"{self.id}, {self.name}, {self.grade}, {self.balance}, {self.role}, {self.email}, {self.hashed_password}"
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, url_for, redirect, request, session, a
 from data import db_session
 from data.dishes import Dishes
 from data.users import User
+from data.application import Application
+import pygame
 from forms.user import RegisterForm, LoginForm
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+import requests
+import sys
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -39,7 +44,11 @@ def reqister():
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Такой пользователь уже есть")
-        user = User(
+        elif db_sess.query(Application).filter(Application.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = Application(
             name=form.name.data,
             email=form.email.data,
             grade=form.grade.data,
@@ -73,7 +82,7 @@ def balance():
     if request.method == 'POST':
         amount = request.form['amount']
         db_sess = db_session.create_session()
-        balance = db_sess.query(User).filter(User.id).first()
+        balance = db_sess.query(User).filter(User.id == current_user.id).first()
         balance.balance += int(amount)
         db_sess.commit()
         return redirect('/')
@@ -85,14 +94,95 @@ def inform():
     return render_template('inform.html')
 
 
+@app.route("/add_new_user")
+def add():
+    if current_user.role == 'student' or current_user.role == 'cook':
+        return redirect('/')
+    else:
+        db_sess = db_session.create_session()
+        application = db_sess.query(Application)
+        return render_template("application.html", application=application)
+
+
+@app.route('/order')
+def order():
+    if current_user.role == 'student':
+        return redirect('/')
+    else:
+        teacher_grade = current_user.grade
+        db_sess = db_session.create_session()
+        data = db_sess.query(User).filter(User.grade == teacher_grade, User.role == 'student').all()
+        return render_template("order.html", data=data)
+
+
+@app.route('/add/<int:id>')
+def add_user(id):
+    db_sess = db_session.create_session()
+    data = db_sess.query(Application).filter(Application.id == id).first()
+    user = User()
+    user.name = data.name
+    user.email = data.email
+    user.grade = data.grade
+    user.role = data.role
+    user.hashed_password = data.hashed_password
+    user.balance = data.balance
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = db_sess.query(Application).filter(Application.id == id).first()
+    db_sess.delete(user)
+    db_sess.commit()
+    return redirect('/add_new_user')
+
+
+@app.route('/reject/<int:id>')
+def reject(id):
+    db_sess = db_session.create_session()
+    user = db_sess.query(Application).filter(Application.id == id).first()
+    db_sess.delete(user)
+    db_sess.commit()
+    return redirect('/add_new_user')
+
+
 @app.route('/profile')
 @login_required
 def profile():
     name = current_user.name
     name = "_".join(name.split())
     grade = current_user.grade
-    balance = current_user.balance
-    return render_template('profile.html', name=name, balance=balance, grade=grade)
+    return render_template('profile.html', name=name, grade=grade)
+
+
+@app.route('/api')
+def api():
+    map_request = "http://static-maps.yandex.ru/1.x/?ll=37.587093,55.7339748&spn=0.002,0.002&l=map"
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    pygame.display.set_caption('Яндекс офис(улица Льва Толстого, 16, Москва, 119021)')
+    # Рисуем картинку, загружаемую из только что созданного файла.
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    # Переключаем экран и ждем закрытия окна.
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+
+    # Удаляем за собой файл с изображением.
+    os.remove(map_file)
+    return redirect('/')
 
 
 @app.route('/')
